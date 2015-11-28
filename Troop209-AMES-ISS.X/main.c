380855f33dcd7d6907e03ac2bbb1e429d0dffe2c
Ken Yu <kyu_23@yahoo.com>
2015-11-28 12:05:24 -0600

    Update code to synch with Norm's calculation.  Specifically the first two positions of (-90 and 0).
    8) We can now define the exact values top be placed in the registers:
        OC1RS         period        10,000 (decimal) or 0x2710 (hexadecimal)
        OC1R            min (-90)        250        "              0x00FA        "
        OC1R            center (0)       375        "              0x0177        "
        OC1R            max (+90)      500        "              0x01F4         "
    The OC1CONx register will need to have the presale set to 64 instead of the current divide by 256 value.

---
+++ Troop209-AMES-ISS.X/main.c
@@ -0,0 +1,10 @@
+int main(void)
+{
+    // initialize all modules
+    //kernel01();
+    //kernel02();
+    //kernel03();  
+    //kernel04();  
+    //kernel05();  
+    kernel07();  
+}
--- Troop209-AMES-ISS.X/src/troop209/servomotor.c
+++ Troop209-AMES-ISS.X/src/troop209/servomotor.c
@@ -5,33 +5,30 @@
 
 int servomortor(void)  
 {
-    // initialize all modules
+   // initialize all modules
     
     system.init();
+    pwm1.init(4);  //
     
-    // Per Norm's email on 11/14
-    
-    // -90 degrees
-    pwm1.initTimer1(10000,250);
-    timer1.start( TIMER_PRE_SCALE_64, 0);  
-    pwm1.turnOnPwm1();    
-    delay(10000);    
-    pwm1.turnOff();
+    timer1.start( 2, 0);  // Set to 1ms clock and using 24 prescaler not 64
+//    pwm1.turnOn( 60000, 50);
+    pwm1.turnOn( 10000, 250);
+    delay(1000);
+//    pwm1.turnOff();
+//    delay(1000);
 
+    pwm1.turnOn( 10000, 375);
+    delay(1000);
+ //   pwm1.turnOff();
+ //   delay(1000);
+    
+    pwm1.turnOn( 10000, 500);
     delay(10000);
+
+//    pwm1.turnOff();
     
-    // 0 degrees
-    pwm1.initTimer1(10000, 375);
-    pwm1.turnOnPwm1();
-    delay(10000);    
-    
-    
-    // TODO:  Add turn 90 degrees with value
-    // pwm1.initTimer1(10000, 500);
-    // pwm1.turnOnPwm1();
-    // delay(10000);    
-    return (0);
-}
+//    delay(5000);
+    return (0);}
 
 //void showCode(int code) {
 //    while(1) flash(code);
