e4be4b465f492ce34fbad0abb6b1eeefc64bb556
kyung <kyung@192.168.92.132>
2016-05-08 04:14:31 -0500

    Merge demo code into baseline

---
+++ Troop209-AMES-ISS.X/build/Troop209-NESI-AMES/main.c
@@ -0,0 +1,14 @@
+int main(void)
+{
+    // initialize all modules
+    //kernel01();
+    //kernel02();
+    //kernel03();  
+    //kernel04();  
+    //kernel05();  
+    //kernel06();  
+    //kernel07();  
+    //kernel08();  
+    kernel12();  
+    //servomotor2();
+}
