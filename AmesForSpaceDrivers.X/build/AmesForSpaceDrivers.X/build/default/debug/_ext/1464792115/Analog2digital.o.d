59e3e6b6c1a6abfa5926170ea73c3c0a05167f32
shinabro@gmail.com <shinabro@gmail.com>
2017-05-22 03:07:13 -0500

    Removed extra folder

--- AmesForSpaceDrivers.X/build/AmesForSpaceDrivers.X/build/default/debug/_ext/1464792115/Analog2digital.o.d
+++
@@ -1,4 +0,0 @@
-build/default/debug/_ext/1464792115/Analog2digital.o:  \
- ../../src/NormI2C/Analog2digital.c  \
-../../src/core/system.h  \
- ../../src/dev/p24FJ256GB106.h 
--- AmesForSpaceDrivers.X/build/AmesForSpaceDrivers.X/build/default/debug/_ext/1639979382/dateTime.o.d
+++
@@ -1,5 +0,0 @@
-build/default/debug/_ext/1639979382/dateTime.o:  \
-../../src/core/dateTime.c  \
- ../../src/core/system.h  \
-../../src/dev/p24FJ256GB106.h  \
- ../../src/core/dateTime.h 
