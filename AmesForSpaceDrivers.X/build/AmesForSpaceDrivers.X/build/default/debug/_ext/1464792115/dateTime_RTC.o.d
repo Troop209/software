1d7cbc1133e6b7f4eecc8c19a1b26e13f1a85c55
shinabro@gmail.com <shinabro@gmail.com>
2017-05-22 03:09:09 -0500

    Removed extra folder

--- AmesForSpaceDrivers.X/build/AmesForSpaceDrivers.X/build/default/debug/_ext/1464792115/dateTime_RTC.o.d
+++
@@ -1,7 +0,0 @@
-build/default/debug/_ext/1464792115/dateTime_RTC.o:  \
- ../../src/NormI2C/dateTime_RTC.c  \
-../../src/core/system.h  \
- ../../src/dev/p24FJ256GB106.h  \
-../../src/NormI2C/dateTime_RTC.h  \
- ../../src/core/datetime.h  \
-../../src/core/system.h 
--- AmesForSpaceDrivers.X/build/AmesForSpaceDrivers.X/build/default/debug/_ext/1639979382/resistiveSensors.o.d
+++
@@ -1,5 +0,0 @@
-build/default/debug/_ext/1639979382/resistiveSensors.o:  \
- ../../src/core/resistiveSensors.c  \
-../../src/core/system.h  \
- ../../src/dev/p24FJ256GB106.h  \
-../../src/core/resistiveSensors.h 
